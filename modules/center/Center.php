63bf35ab86a68ebd4b2d38475d6e6fa94b91292c
ruoshuiqinmo0 <609392790@qq.com>
2018-07-31 14:09:14 +0800

    frist commit

---
+++ modules/center/Center.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\center;
+
+/**
+ * center module definition class
+ */
+class Center extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\center\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
